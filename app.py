ee016359d580e461eab8d210891d11e8ab166d48
LeTheDuy-hihi <theduyduy2006@gmail.com>
2026-06-04 17:12:17 +0700

    Revert camera mirror toggle

--- app.py
+++ app.py
@@ -1035,25 +1035,12 @@ def show_main_app():
             
             uploaded_file = None
             if img_source == "📸 Chụp ảnh từ Camera":
-                mirror_cam = st.checkbox("🔄 Lật Camera (Mirror Mode)", value=False, help="Chế độ soi gương")
-                if not mirror_cam:
-                    st.markdown("""
-                    <style>
-                        video {
-                            transform: scaleX(1) !important;
-                            -webkit-transform: scaleX(1) !important;
-                        }
-                    </style>
-                    """, unsafe_allow_html=True)
                 uploaded_file = st.camera_input("Hướng vùng da bị tổn thương vào camera và chụp")
             else:
                 uploaded_file = st.file_uploader("Chọn file ảnh từ máy tính", type=["jpg", "jpeg", "png"])
             
             if uploaded_file:
                 image = Image.open(uploaded_file).convert('RGB')
-                if img_source == "📸 Chụp ảnh từ Camera" and mirror_cam:
-                    from PIL import ImageOps
-                    image = ImageOps.mirror(image)
                 st.image(image, caption="Hình ảnh đã nhận", use_container_width=True)
             st.markdown("</div>", unsafe_allow_html=True)
             
